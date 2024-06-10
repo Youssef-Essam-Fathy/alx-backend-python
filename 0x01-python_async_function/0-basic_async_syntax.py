4d9aa675ab7b78c8f1cf8df6d0691dd45d15c516
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-10 09:03:56 +0300

    basic async syntax

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""Importing asyncio and random modules"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay = 10):
+
+    """wait for printing a random value
+
+    Args:
+        max_delay (int, optional): max delay. Defaults to 10.
+
+    Returns:
+        _type_: _description_
+    """
+
+    await asyncio.sleep(max_delay)
+    return random.uniform(0, max_delay)
