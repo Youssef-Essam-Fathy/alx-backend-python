7ffac72468be1d895ef5166f71752a7ec5be5783
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-11 14:48:11 +0300

    Asynchronous generator that yields random floats between 0 and 10.

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""
+This module contains an asynchronous generator
+that yields random integers between 0 and 10.
+The generator is intended to demonstrate
+the use of asynchronous programming in Python
+to perform non-blocking operations.
+"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Asynchronous generator that yields
+    random floats between 0 and 10.
+
+    This generator produces 10 random floats,
+    each after a 1-second delay.
+    It demonstrates the use of asynchronous
+    generators to perform non-blocking
+    operations while yielding values.
+
+    Yields:
+        int: A random float between 0 and 10 (inclusive).
+
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
