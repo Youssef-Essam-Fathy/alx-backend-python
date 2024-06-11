8026383d3d7a81c761e5d89708a1ef1af677fe2e
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-06-11 15:13:46 +0300

    Asynchronously generates a list of floats using async_generator

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Async comprehension demonstration.
+"""
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Asynchronously generates a list of floats using async_generator.
+    """
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    return result
